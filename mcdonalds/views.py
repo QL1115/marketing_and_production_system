84974883e2b288791764fe76afdc7e2870a857a5
winniechiou <s410349@shsh.tw>
2020-12-09 20:28:24 +0800

    test store_details add template

--- mcdonalds/views.py
+++ mcdonalds/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Sales, RFM, Customers, ShoppingRecords, MarketingStrategies, Products, RawMaterial, StrategyProductRel, ProductMaterialRel, StoreDemand, StoreDemandDetails, MarketingData, Stores, Orders, Suppliers
 
 # Create your views here.
 def index(request):
     return HttpResponse('產銷資訊系統首頁')
+
+def stores_detail(request):
+    stores=Stores.objects.values()
+    return render(request,'mcdonalds/stores_detail.html', {'stores': stores})
+
