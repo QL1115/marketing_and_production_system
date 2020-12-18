bb77b518f7825cd4f6ff9522ff9d986c4e46af1e
winniechiou <s410349@shsh.tw>
2020-12-18 20:33:21 +0800

    上傳原物料預估、原物料實際訂單還有庫存

--- mcdonalds/urls.py
+++ mcdonalds/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'mcdonalds'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('stores_detail',views.stores_detail)
+    path('stores_detail',views.stores_detail),
+    path('raw_materials_predict',views.raw_materials_predict),
+    path('raw_materials_order',views.raw_materials_order),
+    path('raw_materials',views.raw_materials),
+    path('update/＜int:pk＞', views.update_raw_materials, name='update')
 ]
--- mcdonalds/views.py
+++ mcdonalds/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Sales, RFM, Customers, ShoppingRecords, MarketingStrategies, Products, RawMaterial, StrategyProductRel, ProductMaterialRel, StoreDemand, StoreDemandDetails, MarketingData, Stores, Orders, Suppliers
+from django.db import connection
 
 # Create your views here.
 def index(request):
@@ -9,4 +10,52 @@ def index(request):
 def stores_detail(request):
     stores=Stores.objects.values()
     return render(request,'mcdonalds/stores_detail.html', {'stores': stores})
+
+def raw_materials_predict(request):
+    raw_materials_predict=RawMaterial.objects.values('material_name', 'quantity', 'reorder_point').order_by('reorder_point')
+    print(raw_materials_predict)
+    return render(request,'mcdonalds/raw_material_predict.html', {'raw_materials_predict': raw_materials_predict})
+
+def raw_materials_order(request):
+    # raw_materials_order=Orders.objects.all()
+    # raw_materials_order=Orders.objects.values('status', 'order_amount', 'reorder_point')
+    # raw_materials_order=Orders.objects.all().select_related('material_name','order_amount','status','order_date')
+    # raw_materials_order=Orders.objects.all().prefetch_related('material').values('material_name','order_amount','status','order_date')\
+    # #>>> raw_materials_order=Orders.objects.values('order_id','order_amount','status','order_date').filter(order_id=1)
+    cursor2 = connection.cursor()   
+    cursor2.execute("SELECT material_name, order_amount, order_date, status FROM orders INNER JOIN raw_material ON orders.material_id=raw_material.material_id;")
+    raw_materials_order = dictfetchall(cursor2)
+
+    for i in raw_materials_order:
+        if i['status']==0:
+            i['status']='未完成'
+            print(i['status'])
+        else:
+            i['status']='已完成'
+    return render(request,'mcdonalds/raw_materials_order.html', {'raw_materials_order': raw_materials_order})
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+def raw_materials(request):
+    cursor = connection.cursor()   
+    cursor.execute("SELECT material_id,material_name, amount, on_hand_inventory, security_numbers,supplier_name FROM raw_material INNER JOIN suppliers ON raw_material.supplier_id=suppliers.supplier_id;")
+    raw_materials = dictfetchall(cursor)
+    return render(request,'mcdonalds/raw_material.html', {'raw_materials': raw_materials})
+
+
+
+def update_raw_materials(request, pk):
+    raw_materials = RawMaterial.objects.get(material_id=pk)
+
+    form = RawMaterialModelForm(instance=raw_materials)
+    context = {
+        'form': form
+    }
+    return render(request, 'expenses/update_raw_materials.html', context)
 
