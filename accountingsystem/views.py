c9c6b0d9f23790eecc2ac24893984d7426069224
winniechiou <s410349@shsh.tw>
2020-12-26 17:14:49 +0800

    刪除空格 將import_page reload改成安全的方法

--- accountingsystem/views.py
+++ accountingsystem/views.py
@@ -115,38 +115,6 @@ def get_import_page(request,comp_id, rpt_id, acc_id):
         count_Depositaccount_result=check(rpt_id)[1]
 
 
-
-        # #執行原生sql，查詢CashInBanks是否已經有匯入
-        # cursor1 = connection.cursor()
-        # cursor1.execute("select count(*) from `Group` inner join Company on `Group`.grp_id=Company.grp_id inner join Report on Company.com_id=Report.com_id inner join CashInBanks on Report.rpt_id=CashInBanks.rpt_id WHERE Report.rpt_id = %s", [rpt_id])
-        # count_CashInBank = cursor1.fetchone()
-
-        # #執行原生sql，查詢Depositaccount是否已經有匯入
-        # cursor2 = connection.cursor()
-        # cursor2.execute("select count(*) from `Group` inner join Company on `Group`.grp_id=Company.grp_id inner join Report on Company.com_id=Report.com_id inner join Depositaccount on Report.rpt_id=Depositaccount.rpt_id WHERE Report.rpt_id = %s", [rpt_id])
-        # count_Depositaccount = cursor2.fetchone()
-        # print('~~~~~~~!!!!!!!!!!')
-
-        # if count_CashInBank[0]>0:
-        #     #銀行存款已匯入資料
-        #     print('銀行存款已匯入資料')
-        #     count_CashInBank_result={"status_code": 123, "msg": "銀行存款已匯入資料"}
-            
-        # else:
-        #     #銀行存款沒有
-        #     print('銀行存款沒匯入過')
-        #     count_CashInBank_result={"status_code": 456, "msg": "銀行存款沒匯入過"}
-            
-
-        # if count_Depositaccount[0]>0:
-        #     #定期存款已匯入資料
-        #     print('定期存款已匯入資料')
-        #     count_Depositaccount_result={"status_code": 789, "msg": "定期存款已匯入資料"}
-        # else:
-        #     #定期存款沒有
-        #     print('定期存款沒匯入過')
-        #     count_Depositaccount_result={"status_code": 999, "msg": "定期存款沒匯入過"}
-            
         
         return render(request, 'import_page.html',{ 'acc_id': acc_id,
                                                     'comp_id': comp_id,
