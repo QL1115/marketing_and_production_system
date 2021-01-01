65f56d657808301339dde6602544725fa4e4e179
jesscjlee <cjlee.jess@gmail.com>
2021-01-02 01:35:13 +0800

    merge developer

--- mcdonalds/views.py
+++ mcdonalds/views.py
@@ -9,6 +9,7 @@ from django.db import connection
 from .forms import RawMaterialModelForm, MarketingStrategyForm, OrderForm, StoresContactForm, LoginForm
 import plotly.graph_objects as go
 from django.core import serializers
+from django.db.models import Sum
 
 from django.contrib.auth import authenticate, login, logout
 
@@ -366,10 +367,15 @@ def receive_store_demand(request):
         else:
             print('no')
 
-    return render(request,'mcdonalds/store_send_demand.html', {})
+    stores=Stores.objects.all()
+    products=Products.objects.all()
+    return render(request,'mcdonalds/store_send_demand.html', {'stores':stores,'products':products})
+    #return render(request,'mcdonalds/store_send_demand.html', {})
+
 
 def go_to_store_send_demand_page(request):
-    return render(request,'mcdonalds/store_send_demand.html', {})
+    return render(request,'mcdonalds/store_send_demand.html')
+
 
 def create_store_demand(store_id,product_id,product_num):
     # call by receive_store_demand
@@ -468,3 +474,424 @@ def sign_in(request):
         'form': form
     }
     return render(request, 'mcdonalds/login.html', context)
+
+# 行銷Dashboard
+def marketing_dashboard(request):
+    # 前期比較 
+    pre_month_list = ['2019-01-31', '2019-02-28', '2019-03-31', '2019-04-30', '2019-05-31', '2019-06-30', '2019-07-31', '2019-08-31', '2019-09-30', '2019-10-31', '2019-11-30', '2019-12-31']
+    cur_month_list = ['2020-01-31', '2020-02-29', '2020-03-31', '2020-04-30', '2020-05-31', '2020-06-30', '2020-07-31', '2020-08-31', '2020-09-30', '2020-10-31', '2020-11-30', '2020-12-31']
+    monthList = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.']
+    pre_sales_data = Sales.objects.filter(date__contains='2019')
+    cur_sales_data = Sales.objects.filter(date__contains='2020')
+    
+    # 2019各月銷售量(Default為漢堡)
+    pre_sales_num_list = [pre_sales_data.select_related('product').filter(date__contains=mon, product__category__exact='漢堡').aggregate(Sum('numbers'))['numbers__sum'] for mon in pre_month_list]
+    # 2020各月銷售量
+    cur_sales_num_list = [cur_sales_data.select_related('product').filter(date__contains=mon, product__category__exact='漢堡').aggregate(Sum('numbers'))['numbers__sum'] for mon in cur_month_list]
+    
+    # plotly
+    # Create figure
+    comparison_fig = go.Figure()
+
+    comparison_fig.add_trace(
+        go.Scatter(x=monthList, y=pre_sales_num_list, name="2019")) # name -> legend title
+    comparison_fig.add_trace(
+        go.Scatter(x=monthList, y=cur_sales_num_list, name="2020"))
+
+    # Set title
+    comparison_fig.update_layout(
+        title_text="前期比較(漢堡)"
+    )
+
+    # comparison_fig.update_traces(
+        # hoverinfo="name+x+text",
+        # line={"width": 0.5},
+        # marker={"size": 8},
+        # mode="lines+markers",
+        # showlegend=False
+    # )   
+    
+    # Add range slider
+    comparison_fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=3,
+                         label="3m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=False
+            ),
+        ),
+        yaxis=dict(title='銷售量')
+    )
+    comparisonwithprevious_graph = comparison_fig.to_html()
+    
+    
+    # 銷售排行
+    product_category_list = ['漢堡', '點心', '飲料/湯品']
+    product_sales_2019 = [pre_sales_data.select_related('product').filter(product__category__exact=category).aggregate(Sum('numbers'))['numbers__sum'] for category in product_category_list]
+    product_sales_2020 = [cur_sales_data.select_related('product').filter(product__category__exact=category).aggregate(Sum('numbers'))['numbers__sum'] for category in product_category_list]
+    
+    # plotly
+    salesranking_fig = go.Figure()
+    salesranking_fig.add_trace(go.Bar(
+        y=product_category_list,
+        x=product_sales_2019,
+        name='2019',
+        orientation='h',
+        marker=dict(
+            color='rgba(246, 78, 139, 0.6)',
+            line=dict(color='rgba(246, 78, 139, 1.0)', width=3)
+        )
+    ))
+    salesranking_fig.add_trace(go.Bar(
+        y=product_category_list,
+        x=product_sales_2020,
+        name='2020',
+        orientation='h',
+        marker=dict(
+            color='rgba(58, 71, 80, 0.6)',
+            line=dict(color='rgba(58, 71, 80, 1.0)', width=3)
+        )
+    ))
+
+    salesranking_fig.update_layout(
+        barmode='stack',
+        title_text='銷售排行'
+    ) 
+    salesranking_graph = salesranking_fig.to_html()
+    
+    
+    # 各區銷售表現
+    store_region_list = ['北區', '中區', '南區', '東區']
+    product_sales_2019 = [pre_sales_data.select_related('product').filter(store__store_region=region).aggregate(Sum('numbers'))['numbers__sum']/6 for region in store_region_list]
+    product_sales_2020 = [cur_sales_data.select_related('product').filter(store__store_region=region).aggregate(Sum('numbers'))['numbers__sum']/6 for region in store_region_list]
+    
+    # plotly
+    storeperformance_fig = go.Figure()
+    storeperformance_fig.add_trace(go.Bar(
+        y=store_region_list,
+        x=product_sales_2019,
+        name='2019',
+        orientation='h',
+        marker=dict(
+            color='rgba(246, 78, 139, 0.6)',
+            line=dict(color='rgba(246, 78, 139, 1.0)', width=3)
+        )
+    ))
+    storeperformance_fig.add_trace(go.Bar(
+        y=store_region_list,
+        x=product_sales_2020,
+        name='2020',
+        orientation='h',
+        marker=dict(
+            color='rgba(58, 71, 80, 0.6)',
+            line=dict(color='rgba(58, 71, 80, 1.0)', width=3)
+        )
+    ))
+
+    storeperformance_fig.update_layout(
+        barmode='stack',
+        title_text='各區銷售表現'
+    ) 
+    storeperformance_graph = storeperformance_fig.to_html()
+    
+    
+    
+    # CVR
+    marketing_data = MarketingData.objects.all()
+    month_list = [item.date.strftime('%Y-%m-%d') for item in marketing_data]
+    cvr_list = [item.cvr for item in marketing_data]
+    
+    # plotly
+    # Create figure
+    cvr_fig = go.Figure()
+
+    cvr_fig.add_trace(
+        go.Scatter(x=month_list, y=cvr_list))
+
+    # Set title
+    cvr_fig.update_layout(
+        title_text="CVR"
+    )
+
+    # Add range slider
+    cvr_fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=3,
+                         label="3m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=False
+            ),
+            type="date"
+        ),
+        yaxis=dict(title='CVR (%)')
+    )
+    cvr_graph = cvr_fig.to_html()
+    returnDict = {'ComparisonWithPrevious_graph': comparisonwithprevious_graph, 'SalesRanking_graph': salesranking_graph, 'StorePerformance_graph': storeperformance_graph, 'cvr_graph': cvr_graph}
+    return render(request, 'mcdonalds/marketing_dashboard.html', returnDict)
+    
+# 行銷Dashboard視窗
+def marketing_dashboard_windows(request):
+    categoryDict = {'hamburger': '漢堡', 'snack': '點心', 'beverageNsoup': '飲料/湯品'}
+    pre_sales_data = Sales.objects.filter(date__contains='2019')
+    cur_sales_data = Sales.objects.filter(date__contains='2020')
+    
+    graph =  request.GET['graph']
+    
+    # 前期比較
+    if graph == 'ComparisonWithPrevious':
+        hamburgerDict = {'1': '大麥克', '2': '雙層牛肉吉事堡', '3': '嫩煎雞腿堡', '4': '麥香雞', '5': '勁辣雞腿堡', '6': '黃金起司豬排堡', '7': '麥香魚', '8': '煙燻雞肉長堡', '9': '薑燒豬肉長堡', '10': 'BLT 安格斯黑牛堡',
+                    '11': 'BLT 辣脆雞腿堡', '12': 'BLT 嫩煎雞腿堡', '13': '蕈菇安格斯黑牛堡', '14': '金銀招財薯來堡 (牛)', '15': '金銀招財薯來堡 (雞)', '16': '金銀招財福堡', '17': '法式芥末香雞堡', '18': '漢堡', '19': '吉事漢堡'}
+        snackDict = {'1': '麥克雞塊', '2': '麥脆雞腿', '3': '麥脆雞翅', '4': '凱薩脆雞沙拉', '5': '義式烤雞沙拉', '6': '快樂兒童餐', '7': '搖搖樂雞腿排', '8': '勁辣香雞翅', '9': '酥嫩雞翅', '10': '蘋果派',
+                     '11': '水果袋', '12': '薯餅', '13': '海苔搖搖粉', '14': '蔥辣搖搖粉', '15': '薯條', '16': '四季沙拉', '17': 'OREO冰炫風', '18': '蛋捲冰淇淋', '19': '大蛋捲冰淇淋'}
+        beverageNsoupDict = {'1': '玉米湯', '2': '熱紅茶', '3': '熱奶茶', '4': '鮮乳', '5': '可口可樂', '6': '可口可樂 zero', '7': '雪碧', '8': '冰紅茶 (檸檬風味)', '9': '冰紅茶 (無糖)', '10': '冰綠茶 (無糖)', '11': '冰奶茶', '12': '柳橙汁'}
+        
+        category = request.GET['category']
+        selectedItem = request.GET['selectedItem']
+        
+        if category == 'hamburger':
+            selected = hamburgerDict.get(selectedItem)
+        elif category == 'snack':
+            selected = snackDict.get(selectedItem)
+        elif category == 'beverageNsoup':
+            selected = beverageNsoupDict.get(selectedItem)
+        
+        # 前期比較 
+        pre_month_list = ['2019-01-31', '2019-02-28', '2019-03-31', '2019-04-30', '2019-05-31', '2019-06-30', '2019-07-31', '2019-08-31', '2019-09-30', '2019-10-31', '2019-11-30', '2019-12-31']
+        cur_month_list = ['2020-01-31', '2020-02-29', '2020-03-31', '2020-04-30', '2020-05-31', '2020-06-30', '2020-07-31', '2020-08-31', '2020-09-30', '2020-10-31', '2020-11-30', '2020-12-31']
+        month_list = pre_month_list + cur_month_list
+        
+        # 2019各月銷售量
+        pre_sales_num_list = [pre_sales_data.select_related('product').filter(date__contains=mon, product__product_name__contains=selected).aggregate(Sum('numbers'))['numbers__sum'] for mon in pre_month_list]
+        # 2020各月銷售量
+        cur_sales_num_list = [cur_sales_data.select_related('product').filter(date__contains=mon, product__product_name__contains=selected).aggregate(Sum('numbers'))['numbers__sum'] for mon in cur_month_list]
+        
+        # plotly
+        # Create figure
+        comparison_window_fig = go.Figure()
+
+        comparison_window_fig.add_trace(
+            go.Scatter(x=month_list, y=pre_sales_num_list, name="2019")) # name -> legend title
+        comparison_window_fig.add_trace(
+            go.Scatter(x=month_list, y=cur_sales_num_list, name="2020"))
+
+        # Set title
+        comparison_window_fig.update_layout(
+            title_text=selected
+        )
+
+        # comparison_fig.update_traces(
+            # hoverinfo="name+x+text",
+            # line={"width": 0.5},
+            # marker={"size": 8},
+            # mode="lines+markers",
+            # showlegend=False
+        # )   
+        
+        # Add range slider
+        comparison_window_fig.update_layout(
+            xaxis=dict(
+                rangeselector=dict(
+                    buttons=list([
+                        dict(count=1,
+                             label="1m",
+                             step="month",
+                             stepmode="backward"),
+                        dict(count=3,
+                             label="3m",
+                             step="month",
+                             stepmode="backward"),
+                        dict(count=6,
+                             label="6m",
+                             step="month",
+                             stepmode="backward"),
+                        dict(count=1,
+                             label="1y",
+                             step="year",
+                             stepmode="backward"),
+                        dict(step="all")
+                    ])
+                ),
+                rangeslider=dict(
+                    visible=True
+                ),
+            ),
+            yaxis=dict(title='銷售量')
+        )
+        
+        window_graph = comparison_window_fig.to_html()
+    # 銷售排行
+    elif graph == 'SalesRanking':
+        category = request.GET['category']
+        
+        product_sales_2019 = [pre_sales_data.select_related('product').filter(product__category__exact=categoryDict.get(category)).values('product__product_name').annotate(total=Sum('numbers'))][0]
+        total_2019 = [x.get('total') for x in product_sales_2019]
+        product_2019 = [x.get('product__product_name') for x in product_sales_2019]
+
+        product_sales_2020 = [cur_sales_data.select_related('product').filter(product__category__exact=categoryDict.get(category)).values('product__product_name').annotate(total=Sum('numbers'))][0]
+        total_2020 = [x.get('total') for x in product_sales_2020]
+        product_2020 = [x.get('product__product_name') for x in product_sales_2020]
+        
+        # plotly
+        salesranking_window_fig = go.Figure()
+        salesranking_window_fig.add_trace(go.Bar(
+            y=product_2019,
+            x=total_2019,
+            name='2019',
+            orientation='h',
+            marker=dict(
+                color='rgba(246, 78, 139, 0.6)',
+                line=dict(color='rgba(246, 78, 139, 1.0)', width=3)
+            )
+        ))
+        salesranking_window_fig.add_trace(go.Bar(
+            y=product_2020,
+            x=total_2020,
+            name='2020',
+            orientation='h',
+            marker=dict(
+                color='rgba(58, 71, 80, 0.6)',
+                line=dict(color='rgba(58, 71, 80, 1.0)', width=3)
+            )
+        ))
+
+        salesranking_window_fig.update_layout(
+            barmode='stack',
+            title_text='銷售排行',
+            autosize=False,
+            width=800,
+            height=550,
+            yaxis={'categoryorder':'total ascending'}  
+
+        ) 
+        window_graph = salesranking_window_fig.to_html()
+    # 各店銷售表現
+    elif graph == 'StorePerformance':
+        regionDict = {'north': '北區', 'central': '中區', 'south': '南區', 'east': '東區'}
+        region = request.GET['region']
+        
+        product_sales_2019 = [pre_sales_data.select_related('store').filter(store__store_region=regionDict.get(region)).values('store__store_name').annotate(total=Sum('numbers'))][0]
+        total_2019 = [x.get('total') for x in product_sales_2019]
+        store_2019 = [x.get('store__store_name') for x in product_sales_2019]
+        
+        product_sales_2020 = [pre_sales_data.select_related('store').filter(store__store_region=regionDict.get(region)).values('store__store_name').annotate(total=Sum('numbers'))][0]
+        total_2020 = [x.get('total') for x in product_sales_2020]
+        store_2020 = [x.get('store__store_name') for x in product_sales_2020]
+        
+        # plotly
+        storeperformance_window_fig = go.Figure()
+        storeperformance_window_fig.add_trace(go.Bar(
+            y=store_2019,
+            x=total_2019,
+            name='2019',
+            orientation='h',
+            marker=dict(
+                color='rgba(246, 78, 139, 0.6)',
+                line=dict(color='rgba(246, 78, 139, 1.0)', width=3)
+            )
+        ))
+        storeperformance_window_fig.add_trace(go.Bar(
+            y=store_2020,
+            x=total_2020,
+            name='2020',
+            orientation='h',
+            marker=dict(
+                color='rgba(58, 71, 80, 0.6)',
+                line=dict(color='rgba(58, 71, 80, 1.0)', width=3)
+            )
+        ))
+
+        storeperformance_window_fig.update_layout(
+            barmode='stack',
+            title_text='各店銷售表現'
+        ) 
+        window_graph = storeperformance_window_fig.to_html()
+        
+    # CVR
+    elif graph == 'CVR':
+        marketing_data = MarketingData.objects.all()
+        month_list = [item.date.strftime('%Y-%m-%d') for item in marketing_data]
+        cvr_list = [item.cvr for item in marketing_data]
+        
+        # plotly
+        # Create figure
+        cvr_fig = go.Figure()
+
+        cvr_fig.add_trace(
+            go.Scatter(x=month_list, y=cvr_list))
+
+        # Set title
+        cvr_fig.update_layout(
+            title_text="CVR"
+        )
+
+        # Add range slider
+        cvr_fig.update_layout(
+            xaxis=dict(
+                rangeselector=dict(
+                    buttons=list([
+                        dict(count=1,
+                             label="1m",
+                             step="month",
+                             stepmode="backward"),
+                        dict(count=3,
+                             label="3m",
+                             step="month",
+                             stepmode="backward"),
+                        dict(count=6,
+                             label="6m",
+                             step="month",
+                             stepmode="backward"),
+                        dict(count=1,
+                             label="1y",
+                             step="year",
+                             stepmode="backward"),
+                        dict(step="all")
+                    ])
+                ),
+                rangeslider=dict(
+                    visible=True
+                ),
+                type="date"
+            ),
+            yaxis=dict(title='CVR (%)')
+        )
+        window_graph = cvr_fig.to_html()
+    return JsonResponse({'WindowGraph': window_graph})
