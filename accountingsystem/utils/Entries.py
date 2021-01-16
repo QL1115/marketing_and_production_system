558b6b2f2de1f31c22add15a9fc984ecda01500c
cozy <whatamath0626@gmail.com>
2021-01-16 16:02:59 +0800

    修正Entries

--- accountingsystem/utils/Entries.py
+++ accountingsystem/utils/Entries.py
@@ -14,14 +14,13 @@ def create_preamount_and_adjust_entries_for_project_account(comp_id, rpt_id, acc
     create_adjust_entries(comp_id, rpt_id, acc_id) 
 
 def create_preamount(comp_id, rpt_id, acc_id):
-    #如果科目是現金，建立現金的調整表
-    if acc_id==4:
-        #create_cash_preamount(rpt_id, acc_id)
+    if acc_id==1:
+        create_cash_preamount(rpt_id, acc_id)
         print('create_preamount')
 
 def create_adjust_entries(comp_id, rpt_id, acc_id):
     #如果科目是現金，建立現金的分錄
-    if acc_id==4:
+    if acc_id==1:
         create_cash_adjust_entries(rpt_id, acc_id)
         print('create_adjust_entries')
 
